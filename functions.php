3806d4f0496c7a1554057efb2a21d155f2a5f02f
David Kirby <davidkirby@Davids-MBP.attlocal.net>
2022-09-16 22:29:17 -0500

    fixed functions.php

--- functions.php
+++ functions.php
@@ -1,41 +1,60 @@
+
 <?php
 
-add_theme_support('title-tag');
-add_theme_support('custom-logo');
-add_theme_support('post-thumbnails');
 
-function ME_styles() {
-    wp_enqueue_style('theme-style', get_template_directory_url() . "/style.css", array(), '1.0');
-}
+/** load stylesheets */
+   function ME_styles() {
 
-add_action('wp_enqueue_scripts', 'ME_styles');
+    wp_enqueue_style('theme-style', get_template_directory_uri() . "/style.css", array(), '1.3');
+
+   }
+
+   add_action('wp_enqueue_scripts', 'ME_styles');
 
 
-function register_menus() {
-    register_nav_menus(
+  
 
-        array (
+
+   /** menus */
+  
+   function register_menus() {
+      register_nav_menus(
+
+         array (
             'header-menu' => 'Header Nav',
             'side-menu' => 'Side Nav',
             'footer-menu' => 'Footer Nav',
-        )
-        );
-}
+         )
+         );
+   }
 
-add_action( 'init' , 'register_menus' );
+   add_action( 'init' , 'register_menus');
+
+   $defaults = array(
+      'default-color'          => '',
+      'default-image'          => '',
+      'default-repeat'         => '',
+      'default-position-x'     => '',
+      'default-attachment'     => '',
+      'wp-head-callback'       => '_custom_background_cb',
+      'admin-head-callback'    => '',
+      'admin-preview-callback' => ''
+  );
+
+  add_theme_support( 'custom-background', $defaults );
 
 
-$defaults = array(
-    'default-color'          => '',
-    'default-image'          => '',
-    'default-repeat'         => '',
-    'default-position-x'     => '',
-    'default-attachment'     => '',
-    'wp-head-callback'       => '_custom_background_cb',
-    'admin-head-callback'    => '',
-    'admin-preview-callback' => ''
-);
+/** add featured image option */
+  add_theme_support('post-thumbnails');
 
-add_theme_support( 'custom-background', $defaults );
+  add_image_size('small-thumbnail', 320, 240, false);
+
+
+/** enable theme options */
+
+
+   add_theme_support('title-tag');
+   add_theme_support('custom-logo');
+   add_theme_support('html5', array('comment-list', 'comment-form', 'search-form', 'gallery', 'caption'))
 
 ?>
